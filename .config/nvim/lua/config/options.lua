b3f03f1543fcfc7c72a35e640e526cc610150f41
Aaron Bushnell <aabushnell@gmail.com>
2025-01-30 19:00:19 +0100

    add nvim config

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,4 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.python3_host_prog = "/home/aaron/.pyenv/versions/py3nvim/bin/python"
